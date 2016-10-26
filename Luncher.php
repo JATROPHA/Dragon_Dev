d8744c74c00903ee74ca956c537a646ba3403e6a
JATROPHA <Jat.m7667@gmail.com>
2016-10-26 15:52:22 +0330

    lunch.up

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '239767461:AAFIf9UHRmhJg0pw81zFujxDdI-bY8GBDUc');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
